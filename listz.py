b828b548c99558c3ebb7753738cc8a342da1515e
BhanuGande <bhanu.beloved@gmail.com>
2018-09-14 09:25:13 +0530

    changed one line

--- listz.py
+++ listz.py
@@ -6,27 +6,7 @@
 # for a in l4:
 # 	print(a)
 
-# list compreshension
-# a = 'DigtialLync'
-# l = list(a)
-# print(l)
-
 # l = []
 # for  a in range(0,11):
 # 	# print(a)
-# 	l.append(a)
-# print(l)
-
-# d = 'DigtialLync'
-# l = [a for a in d]
-# print(l)
-
-
-# l = [a for a in range(0,11)]
-# print(l)
-
-l = [1,2,3,['apple','Nokia','samsung',[44,66,88]],56,334]
-
-print(l[3])
-print(l[3][2])
-print(l[3][3][2])
+# 	l.a
